ba20f72726cfe47d70a315cb99da546b2992f256
meia-yong <meiayong19@gmail.com>
2026-07-09 18:26:13 +1000

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from survival_analysis import run_survival_analysis
+from poisson_model import run_poisson_model
+
+def main():
+    run_survival_analysis()
+    run_poisson_model()
+
+if __name__ == "__main__":
+    main()
